d5b3f00c466410899ee4d35b4989aaf96178a6cc
Thamara <sllakshancc@hotmail.com>
2024-05-04 13:31:58 +0530

    chat

---
+++ chat.py
@@ -0,0 +1,106 @@
+from langchain_openai import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain_core.prompts import PromptTemplate
+from langchain_community.vectorstores import FAISS
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.memory import ConversationBufferMemory
+import os
+import shutil
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+OPENAI_KEY = os.getenv("API_KEY")
+
+embeddings = OpenAIEmbeddings(
+    openai_api_key = OPENAI_KEY
+)
+
+
+def load_docs():
+    loader = PyPDFLoader("data/row_01.pdf")
+    pages = loader.load_and_split()
+    pages = pages[1:] # exlude first page
+    print(f"loaded {len(pages)} pages.")
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=400, chunk_overlap=30)
+    docs = text_splitter.split_documents(pages)
+    print(f"loaded {len(docs)} docs.")
+    db = FAISS.from_documents(docs, embeddings)
+    if os.path.exists("local_vector_store/faiss_index"):
+        shutil.rmtree("local_vector_store/faiss_index")
+        print("Deleted existing vector store")
+    db.save_local("local_vector_store/faiss_index")
+    print("Saved vector store")
+
+
+
+chat_template = """You are a helpful AI assistant that can answer user questions based on the chat history and the given context.
+
+<CONTEXT>:
+{context}
+</CONTEXT>
+
+CHAT HISTORY:
+{chat_history}
+
+User Question: {question}
+
+AI Assistant:"""
+
+chat_prompt = PromptTemplate(
+    input_variables=["context", "chat_history", "question"],
+    template=chat_template
+)
+
+def format_docs(docs):
+    return "\n\n".join(doc[0].page_content for doc in docs)
+
+
+
+vector_db = FAISS.load_local("local_vector_store/faiss_index", embeddings, allow_dangerous_deserialization=True)
+llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, api_key=OPENAI_KEY)
+memory = ConversationBufferMemory(memory_key="chat_history", ai_prefix="AI Assistant")
+
+chat_chain = LLMChain(
+    llm=llm,
+    prompt=chat_prompt,
+    verbose=True
+)
+
+
+def chat(message):
+    if not os.path.exists("local_vector_store/faiss_index"):
+        return "Error: No local vector store found in /local_vector_store"
+    context = vector_db.similarity_search_with_score(message, k=4)
+    context = format_docs(context)
+    chat_history = memory.load_memory_variables({})['chat_history']
+    res = chat_chain.invoke({"question": message, "context": context, "chat_history": chat_history})['text']
+    memory.save_context({"input": message}, {"output": res})
+    return res
+
+
+def main():
+    while True:
+        user_input = input("Question: ").strip()
+
+        if not user_input:
+            print("Error: Input is empty")
+            continue
+
+        if user_input.lower() == "exit":
+            print("Exiting the chat.")
+            break
+
+        response = chat(user_input)
+
+        print("Response: ", response)
+
+        
+
+if __name__ == "__main__":
+    main()
+
+
